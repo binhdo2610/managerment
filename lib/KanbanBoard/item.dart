0b37544dc1f43ebc19cc277afa850144c3b8aec4
binhdo2610 <130138029+binhdo2610@users.noreply.github.com>
2024-07-17 21:48:23 +0700

    new_version#1

--- lib/KanbanBoard/item.dart
+++ lib/KanbanBoard/item.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/foundation.dart';
-
 class Item {
   final String id;
+  String listId;
   final String title;
   final String? description;
-   int status;
+  final String status;
   final DateTime createdAt;
   final DateTime updatedAt;
   final String userId;
 
   Item({
     required this.id,
+    required this.listId,
     required this.title,
     this.description,
     required this.status,
@@ -18,15 +18,4 @@ class Item {
     required this.updatedAt,
     required this.userId,
   });
-  factory Item.fromJson(Map<String, dynamic> json) {
-    return Item(
-      id: json['id'],
-      title: json['title'],
-      description: json['description'],
-      status: json['status'],
-      createdAt: DateTime.parse(json['createdAt']),
-      updatedAt: DateTime.parse(json['updatedAt']),
-      userId: json['userId'],
-    );
-  }
 }
